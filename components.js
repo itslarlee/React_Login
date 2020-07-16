ead1a29b032d7895b71edab288a08060e42f428e
Leeren <leerenwus1@gmail.com>
2020-07-16 10:35:36 -0600

    Going to change folders name

--- components.js
+++ components.js
@@ -1,5 +1,5 @@
 
-class Boton extends React.Component {
+class App extends React.Component {
 
   constructor(props) { //Es el metodo de las clases que nos permiten construir datos, y el unico valor que toma son las propiedades. 
       super(props); //super es un metodo para quitar heredacion y que las propiedades de la clase React component no se le pase a nuestro componente
@@ -9,31 +9,20 @@ class Boton extends React.Component {
   render(){//metodo que nos dice que tenemos que mostrar
       return (
       <div>
-          <button>{this.props.texto/*Esto sirve para que cada  boton sea independiente*/ }</button>
-          <Hello msg='Hola como esta'/>
-          <Hello msg='She said koniqiwa'/>
+          <InputTitle label="Name"/>
+          <FormInput/>
+          <InputTitle label="Username"/>
+          <FormInput/>
+          <InputTitle label="Email"/>
+          <FormInput/>
+          <InputTitle label="password"/>
+          <FormInput/>
       </div>
       )
   }
 
 
 }
-
-class Hello extends React.Component{
-  constructor(props) {
-      super(props);
-  }
-
-  render() {
-      return(
-          <h1>{this.props.msg}</h1>
-      )
-  }
-
-
-
-}
-
 
 class InputTitle extends React.Component {
   constructor(props) {
@@ -59,6 +48,6 @@ class SubmitButton extends React.Component {
 
 
 
-ReactDOM.render(<Boton texto='Holaa'/>, document.getElementById("app"));
+ReactDOM.render(<App/>, document.getElementById("app"));
 
 
